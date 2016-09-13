7834be6c947792a63f1db892e50bcc505f778074
雷窑平 <leiyaoping123@163.com>
2016-09-13 16:58:01 +0800

    Initial Commit

---
+++ Leftslider/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Leftslider
+//
+//  Created by 雷窑平 on 16/9/13.
+//  Copyright © 2016年 雷窑平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Leftslider/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Leftslider
+//
+//  Created by 雷窑平 on 16/9/13.
+//  Copyright © 2016年 雷窑平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
